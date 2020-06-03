cc643c46f9f804e39d4075770413ed440eb2069e
RunaticMoon <mwngh3@gmail.com>
2020-06-03 17:29:11 +0900

    [기능] Cognito Trigger 이전 완료

--- modules/neo4j/query.ts
+++ modules/neo4j/query.ts
@@ -1,4 +1,5 @@
 export const enum Query {
+  create_user = 'CREATE (:User {id: $uid, nickname: $nickname, email: $email, profile: $profile, created_at: DATETIME()})',
   create_post_instagram = 'CREATE (:Post:Instagram {id: $key, img_url: $img_url, content: $content, likes: $likes, date: $date})',
   update_post = `MATCH (p:Post {id: $pid}) 
                 CALL apoc.create.setProperties(p, $keys, $values) YIELD node 
