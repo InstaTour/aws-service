129c0ca94131280ebc4208173d618d7b01c398a5
RunaticMoon <mwngh3@gmail.com>
2020-06-10 16:27:14 +0900

    [기능] 게시글에 views 속성 추가

--- modules/neo4j/query.ts
+++ modules/neo4j/query.ts
@@ -1,6 +1,6 @@
 export const enum Query {
-  create_post_instagram = 'CREATE (:Post:Instagram {id: $key, img_url: $img_url, content: $content, likes: $likes, date: $date})',
   create_user = 'CREATE (:User {id: $id, nickname: $nickname, email: $email, profile: $profile, posting: 0, created_at: DATETIME(), updated_at: DATETIME()})',
+  create_post_instagram = 'CREATE (:Post:Instagram {id: $key, img_url: $img_url, content: $content, likes: $likes, views: 0, date: $date })',
   update_post = `MATCH (p:Post {id: $pid}) 
                 CALL apoc.create.setProperties(p, $keys, $values) YIELD node 
                 RETURN node`,
