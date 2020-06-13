8336e78371813bc2888a225f8d2925da55dbbc3d
RunaticMoon <mwngh3@gmail.com>
2020-06-13 21:47:51 +0900

    [버그] uid 변수가 id로 바뀌어 있던 오류 해결

--- modules/neo4j/query.ts
+++ modules/neo4j/query.ts
@@ -1,5 +1,5 @@
 export const enum Query {
-  create_user = 'CREATE (:User {id: $id, nickname: $nickname, email: $email, profile: $profile, posting: 0, created_at: DATETIME(), updated_at: DATETIME()})',
+  create_user = 'CREATE (:User {id: $uid, nickname: $nickname, email: $email, profile: $profile, posting: 0, created_at: DATETIME(), updated_at: DATETIME()})',
   create_post_instagram = 'CREATE (:Post:Instagram {id: $key, img_url: $img_url, content: $content, likes: $likes, views: 0, date: $date })',
   update_post = `MATCH (p:Post {id: $pid}) 
                 CALL apoc.create.setProperties(p, $keys, $values) YIELD node 
